64d27321780bf7a790d067407ab2809ef7211982
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2024-01-21 10:55:51 +0530

    Add files via upload

---
+++ basic-javascript/index.js
@@ -0,0 +1,214 @@
+// console.log('javascript')
+
+
+// alert('hello'); alert('world');
+
+
+// alert('hello')
+// alert('world')
+
+
+/* alert(10 +
+     20
+    + 30); */
+
+
+// Variables
+
+//named storage
+
+// let user; //declaration
+//  user = 'john'; //initialization
+
+//  let user1 = 'peter';
+
+
+
+// let name = 'john';
+// let age = 20;
+// let country = 'india';
+
+// let name = 'john', age = 20, country = 'india';
+
+
+// console.log(name, age, country);
+// var name = 'john', age = 20, country = 'india';
+// console.log(name, age, country);
+
+
+//variable name limitations
+
+//first char must not be a digit
+//only letters, digits, $ and _ are allowed
+
+//allowed
+// let userName
+
+// let test222
+
+// let $
+
+// let _
+
+//casing matters
+//var name and var NAME are two diff variables
+//dont use reserved keywords for variable names like var let return function class etc
+
+// "use strict";
+
+// num = 10;
+
+// alert(num);
+
+
+//constants
+
+const userBirthday = '20/02/1994';
+
+//reassignment not possible
+
+//upper constants
+
+// const WHITE_COLOR = '#FFFFFF';
+
+
+// const BLUE_COLOR = '#00F';
+
+
+// let name = 'john';
+
+// alert(name);
+
+
+// name = 20;
+
+// alert(name);
+
+
+//data types
+
+//8 data types in javascript
+
+//javascript is a dynamically typed language
+
+//Number
+
+// let num = 10;
+//  num  = 10.124;
+
+//special numerical values =>Infinity, -Infinity, NaN
+
+// alert(10 / 0);
+
+// alert(Infinity);
+
+
+//computation error
+
+// alert('hello world' / 10);
+
+// alert(NaN + 200);
+
+// alert(NaN ** 0); //exception
+
+//BigInt
+
+// > (2^53 - 1 ) or < -(2^53 - 1 )
+
+// const bigInt = 3616363216361232163812763638216312638216382163286n;
+
+// console.log(typeof bigInt)
+
+
+//String
+
+// let msg = 'hello';
+
+// let msg2 = "world";
+
+// let msg3 = `the sum is of 1 + 2 is 3`;
+
+//backticks => extended functionality
+
+// let name = 'john';
+
+// console.log(`My name is ${name}`);
+
+// console.log(`the sum is of 1 + 2 is ${1 + 2}`);
+
+
+//Boolean (logical)
+
+//true and false
+
+// let isEmailFieldChecked = true;
+
+// console.log(10 > 20)
+
+
+//null
+
+//means nothing, empty or a value unknown
+
+// let age = null;
+
+
+//undefined
+
+//undefined => value is not assigned
+
+// let age;
+
+// console.log(age);
+
+// let age = 20;
+
+// age = null;
+// console.log(age);
+
+
+
+//Objects and symbols
+
+//all the other previous types are called as primitive data types
+
+//primitive datatype => the can contain a value only of a single data type for ex a string or a num etc
+
+//objects are used to store collection of different types
+
+//symbol => create unique identifiers for objects. 
+
+// console.log(typeof undefined);
+
+// console.log(typeof 0);
+
+// console.log(typeof 100n);
+
+// console.log(typeof true);
+
+// // console.log(typeof 'hello');
+
+// console.log(typeof ('hello'));
+
+
+// console.log(typeof Symbol('id'));
+
+// console.log(typeof Math);
+
+// console.log(typeof null);
+
+// console.log(typeof alert);
+
+
+// topics for next week home work
+
+//Basic operators
+//logical operators
+//conditional
+//ternary and shortcitcuit operators
+//loops => for while do while
+//switch statement
+//basic of function
+//arrays
+//objects
+
