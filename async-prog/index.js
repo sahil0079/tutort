e01919eabbb9a63c425d496bb1bc386624428c0b
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2024-02-11 10:50:26 +0530

    Add files via upload

---
+++ async-prog/index.js
@@ -0,0 +1,87 @@
+
+
+// function add(a , b){
+
+//     return a + b
+// }
+
+// add() //100ms
+// // taking 4 secs in time
+
+// //100 more lines of code
+
+
+
+// function sum(a, b) {
+//     console.log(a + b)
+// }
+// console.log('hello world');
+
+// setTimeout(function () {
+//     console.log(100);
+// }, 2000);
+
+// sum(10, 20);
+
+
+
+
+// function sum(a, b) {
+//     console.log(a + b)
+// }
+// console.log('hello world');
+
+// setTimeout(function () {
+//     console.log(100);
+// }, 0);
+
+// sum(10, 20);
+
+
+
+// function greetings() {
+//     console.log('Hello')
+
+// }
+
+// function greetings2() {
+//     console.log('Hi ')
+// }
+
+// setTimeout(greetings, 1000);
+// setTimeout(greetings2, 0);
+
+
+// console.log('Bye')
+
+
+// function greetings() {
+//     console.log('Hi there')
+// }
+
+// function runningFor1Sec() {
+//     //blocks the javascript thread
+//     console.log('runningFor1Sec');
+// }
+// setTimeout(greetings, 0);
+// runningFor1Sec();
+// console.log('Bye');
+
+// window.console.log('hello world');
+
+// window.setTimeout(function () {
+//     console.log('timer')
+// }, 2000)
+
+// console.log('hello world');
+
+// setTimeout(function () {
+//     console.log('timer')
+// }, 2000)
+
+
+//settimeout
+//setinterval
+//fetch
+//functions that will return promises
+
